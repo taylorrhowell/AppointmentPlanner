e675b8e285c477da34ea97d46022a0566135b5e1
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2021-06-03 11:03:37 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -9,6 +9,8 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   const ROUTES = {
     CONTACTS: "/contacts",
@@ -19,6 +21,12 @@ function App() {
   Implement functions to add data to
   contacts and appointments
   */
+  const addContacts = (contact) => {
+    setContacts(prevContacts => [contact, ...prevContacts]);
+  };
+  const addAppointments = (appointment) => {
+    setAppointments(prevAppointments => [appointment, ...prevAppointments]);
+  };
 
   return (
     <>
@@ -37,11 +45,11 @@ function App() {
           </Route>
           <Route path={ROUTES.CONTACTS}>
              {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage contacts={contacts} addContacts={addContacts}/>
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
             {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage contacts={contacts} appointments={appointments} addAppointments={addAppointments} />
           </Route>
         </Switch>
       </main>
