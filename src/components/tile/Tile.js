96087797bf527053eed2a07783898fd1c4d34441
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2021-06-03 20:34:31 -0700

    Update Tile.js
    WIP! Can generate a set of <p> elements based on object values, but doesn't distinguish between the first title tile and subsequent tiles.

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({obj}) => {
+  const pData = [];
+  for (const property in obj) {
+    pData.push(`${property}: ${obj[property]}`);
+  };
   return (
     <div className="tile-container">
-      
+      {pData.map(data => <p className="tile">{data}</p>)}
     </div>
   );
 };
