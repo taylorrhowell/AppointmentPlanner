7a7c37593835de6ef227c0a5a7d9ae672371f0a6
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2021-06-08 21:10:23 -0700

    Update AppointmentsPage.js
    Add components needed to render the appointments page. Pass props to next level comps

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,27 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({appointments, contacts, addAppointments}) => {
   /*
   Define state variables for 
   appointment info
   */
+ const [title, setTitle] = useState('');
+ const [contact, setContact] = useState('');
+ const [date, setDate] = useState('');
+ const [time, setTime] = useState('');
+ const resetPage = () => {
+  setTitle('');
+  setContact('');
+  setDate('');
+  setTime('');
+ };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+   addAppointments({title, contact, date, time});
+   resetPage();
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+          contacts={contacts}
+          title={title}
+          setTitle={setTitle}
+          contact={contact}
+          setContact={setContact}
+          date={date}
+          setDate={setDate}
+          time={time}
+          setTime={setTitle}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList appointments={appointments} />
       </section>
     </div>
   );
