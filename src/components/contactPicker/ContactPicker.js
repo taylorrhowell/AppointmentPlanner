988dbf49d145914c639866711bd00e7beb09b7b9
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2021-06-09 21:04:28 -0700

    Update ContactPicker.js
    created basic implementation

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, setContact }) => {
+  const changeContact = ({ target }) => {
+    setContact(target.value);
+  };
   return (
-    ContactPicker
+    <div>
+      <label for="contact-list">Contact</label>
+      <select name="contact" id="contact-list" onChange={changeContact}>
+        <option value="" key='-1' selected="selected">--Please choose an option--</option>
+        {contacts.map( (contact, contactIdx) => {
+          return <option key={contactIdx} value={contact.name}>{contact.name}</option>
+        })}
+      </select>
+    </div>
   );
 };
